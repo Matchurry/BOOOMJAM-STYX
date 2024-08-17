479457cb2152d0129546a870a80904309b60cd09
Matchurry <1218600794@qq.com>
2024-08-17 23:29:37 +0800

    普通关2 已衔接

--- Assets/Scripts/Jump/JumpScenes3.cs
+++ Assets/Scripts/Jump/JumpScenes3.cs
@@ -19,6 +19,23 @@ public class JumpScenes3 : MonoBehaviour
 
     public void Jump()
     {
-        SceneManager.LoadScene(4);
+        //SceneManager.LoadScene(4);
+        SceneManager.LoadSceneAsync(16).completed += (operation) => //启动普通关1
+        {
+            //教程关
+            //没有远程敌人、激光、任何技能
+            GameObject ps = GameObject.Find("Player");
+            Player sc = ps.GetComponent<Player>();
+            sc.level = 1; //场景
+            sc.now_level = 2; // 普通关2
+            sc.isReading = false; //教程提示
+            sc.isShooterAnemy = false; //远程敌人
+            sc.isLazerAttcking = false; //激光敌人
+            sc.isShooterSkill = false; //技能1
+            sc.isSheldSkill = false; //技能2
+            sc.isSheldSkill = false; //技能3
+            sc.CubeInHandLim = 11; //方块上限
+            sc.timeLimitToWin = 180; //坚持时间
+        };
     }
 }
