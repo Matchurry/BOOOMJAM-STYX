697b5b4d0c89ff6fbc9bbcd86b86f5f99b30e734
FFFifteen <750152897@qq.com>
2024-08-18 15:30:53 +0800

    完成了导出游戏项目的设置

--- Assets/ParadoxNotion/NodeCanvas/Modules/StateMachines/FSMState.cs
+++ Assets/ParadoxNotion/NodeCanvas/Modules/StateMachines/FSMState.cs
@@ -43,9 +43,9 @@ namespace NodeCanvas.StateMachines
         }
 
         //just a default orange color
-        public override void OnCreate(Graph assignedGraph) {
-            base.customColor = new Color(1, 0.42f, 0.32f);
-        }
+        //public override void OnCreate(Graph assignedGraph) {
+        //    base.customColor = new Color(1, 0.42f, 0.32f);
+        //}
 
         ///<summary>Declares that the state has finished</summary>
         public void Finish() { Finish(Status.Success); }
